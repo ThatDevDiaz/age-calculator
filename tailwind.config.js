20a4412685902c0a6455d00407d1d542fb244551
Cesar Diaz <dev.cdiaz@gmail.com>
2023-07-12 23:30:35 -0500

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    colors: {
+      "bg-off-white": "hsl(0, 0%, 94%)",
+      "bg-purp": "hsl(259, 100%, 65%)",
+      "purple-text": "hsl(259, 100%, 65%)",
+    },
+
+    extend: {
+      fontSize: {
+        "custom-large": "50px",
+        // Add more custom font sizes as needed
+      },
+      fontWeight: {
+        "custom-extra-bold": 800,
+        // Add more custom font weights as needed
+      },
+    },
   },
   plugins: [],
 };
