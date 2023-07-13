e4706a819bfe93e26554edce8e2a46e9e81db2ce
Cesar Diaz <dev.cdiaz@gmail.com>
2023-07-13 16:54:55 -0500

    tailwind and JS changes

--- src/script.js
+++ src/script.js
@@ -1,18 +1,41 @@
 "use strict";
 
-// For this project I will not be using an API to grab the current date so the calculator will function as if the date is statically set to 01/01/2023 for the sake of testing functionality of the app.
+// For this project I will not be using an API to grab the current date (could not find one that was free and strictly had the date as opposed to other details I did not need/want) so the calculator will function as if the date is statically set to 01/01/2023 for the sake of testing functionality of the app.
 
 const calcAgeBtn = document.querySelector(`button`);
+const errorTextDay = document.querySelector(`#day`);
+const errorTextMonth = document.querySelector(`#month`);
+const errorTextYear = document.querySelector(`#year`);
+const errorMessage = document.querySelector(`#errorMessage`);
+
+let day = document.getElementById(`dayInput`).value;
+let month = document.getElementById(`monthInput`).value;
+let year = document.getElementById(`yearInput`).value;
+
+const errorFunc = function () {
+  errorTextDay.style.color = `red`;
+  errorTextMonth.style.color = `red`;
+  errorTextYear.style.color = `red`;
+  errorMessage.classList.remove(`hidden`);
+};
+
+const errorCheck = function () {
+  if (day > 31) {
+    return errorFunc();
+  } else if (month > 12) {
+    return errorFunc();
+  } else if (year > 2023 || year < 1920) {
+    return errorFunc();
+  } else {
+    let yearAge = 2022 - year;
+    let monthAge = 12 - month;
+    let dayAge = 30 - day;
+    document.getElementById(`yearsOld`).textContent = yearAge;
+    document.getElementById(`monthsOld`).textContent = monthAge;
+    document.getElementById(`daysOld`).textContent = dayAge;
+  }
+};
 
 calcAgeBtn.addEventListener(`click`, function () {
-  let day = document.getElementById(`dayInput`).value;
-  let month = document.getElementById(`monthInput`).value;
-  let year = document.getElementById(`yearInput`).value;
-
-  let yearAge = 2022 - year;
-  let monthAge = 12 - month;
-  let dayAge = 30 - day;
-  document.getElementById(`yearsOld`).textContent = yearAge;
-  document.getElementById(`monthsOld`).textContent = monthAge;
-  document.getElementById(`daysOld`).textContent = dayAge;
+  errorCheck();
 });
